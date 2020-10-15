2faf4228db3fd9582cc6bbfa9fe88a5d6f420e11
Beau324 <Beau.cunningham324@gmail.com>
2020-10-14 20:12:46 -0500

    Initial commit

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcMovies.Models;
+
+namespace MvcMovies.Data
+{
+    public class MvcMoviesContext : DbContext
+    {
+        public MvcMoviesContext(DbContextOptions<MvcMoviesContext> options)
+            :base(options)
+        {
+        }
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
